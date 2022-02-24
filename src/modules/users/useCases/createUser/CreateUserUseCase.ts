bddeb223ca22783555685b249ddab142686e2a4b
FabioSntos <Fabio_santos_2001@outlook.com>
2022-02-23 23:40:52 -0300

    resolucao desafio principios solid

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,17 +1,24 @@
+import { response } from "express";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+	name: string;
+	email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+	constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+	execute({ email, name }: IRequest): User {
+		const userAlreadyExists = this.usersRepository.findByEmail(email);
+		if (userAlreadyExists) {
+			throw new Error("usuario já existe");
+		}
+		this.usersRepository.create({ email, name });
+
+		return this.usersRepository.findByEmail(email);
+	}
 }
 
 export { CreateUserUseCase };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,11 +3,18 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+	constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		const { user_id } = request.params;
+
+		try {
+			const setUserAdmin = this.turnUserAdminUseCase.execute({ user_id });
+			return response.json(setUserAdmin);
+		} catch (error) {
+			response.status(404).json({ error: error });
+		}
+	}
 }
 
 export { TurnUserAdminController };
